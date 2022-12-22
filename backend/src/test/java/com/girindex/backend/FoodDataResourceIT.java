fa2677d9a3220b8de87fb9b4db88927ba82da3ca
Vereb György <verebgeorge@gmail.com>
2022-12-22 23:24:50 +0100

    resolve mapstruct-lombok fight

--- backend/src/test/java/com/girindex/backend/FoodDataResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.girindex.backend;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class FoodDataResourceIT extends FoodDataResourceTest {
-    // Execute the same tests but in packaged mode.
-}
