5e71352bbd833ccf869b6d14823b740139b483e7
Vereb György <verebgeorge@gmail.com>
2022-12-23 18:29:19 +0100

    add lombok annotations to prevent runtime error

--- backend/src/main/java/com/girindex/backend/domain/TimeEntry.java
+++ backend/src/main/java/com/girindex/backend/domain/TimeEntry.java
@@ -1,9 +1,13 @@
 package com.girindex.backend.domain;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-
+@Data
+@Builder
 public class TimeEntry {
     LocalDateTime timestamp;
     double avgPrice;
